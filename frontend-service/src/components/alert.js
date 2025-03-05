e0e748f34c751b85e22779d9be87e5054288fdad
hybristraining-rajesh-admin <rajesh.daundkar@somaiya.edu>
2025-03-05 15:36:39 +0530

    test38

--- frontend-service/src/components/alert.js
+++ frontend-service/src/components/alert.js
@@ -5,7 +5,7 @@ import CheckIcon from '@mui/icons-material/Check';
 export default function SimpleAlert() {
   return (
     <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-      Here is a gentle confirmation that your action was successful.
+      Here is a gentle confirmation that your action was successful. 46e7c174-a3fb-4e65-8bce-26b93489aaa5 
     </Alert>
   );
 }
